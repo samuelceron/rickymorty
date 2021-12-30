3302852526d78fc53fc7fe1a3457ea75fe918619
samuelceron <samceronm@gmail.com>
2021-12-30 12:31:03 -0600

    BE:Separate fetchdatacallback

---
+++ js/fetchDataCallback.js
@@ -0,0 +1,25 @@
+// Function definition and exportation
+export const fetchDataCallback = (url_api, callback) => {
+    //instantiate connection
+    const xhttp = new XMLHttpRequest();
+    //open connection setting the method, route and if it´s asynchronous
+    xhttp.open("GET", url_api, true);
+    //call validation
+    xhttp.onreadystatechange = () => {
+        //state 4 shows it´s finished
+        if (xhttp.readyState === 4) {
+            //state 200 show all´s fine, not 400 or 500
+            if (xhttp.status === 200) {
+                //if all righgt, executes the callback sending the error and the result
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                // if not status 200
+                let error = new Error('Error: ' + url_api);
+                // kill the process sending the error
+                return callback(error, null);
+            }
+        }
+    };
+    //seng the petition
+    xhttp.send();
+};
--- js/index copy.js
+++ js/index copy.js
@@ -33,32 +33,27 @@ function fetchData(url_api, callback){
 // primero buscamos la lista de personajes
 fetchData(api, (error1, data1) => {
   // si error, matamos retornando un error
-  var ids = [0,0,2,4]
-  ids.forEach(element => {
-    console.log(element)
-    if(error1) return console.error(error1);
-    // luego buscamos en la api el id de Rick
-    fetchData(api + data1.results[element].id, (error2, data2) => {
+  if(error1) return console.error(error1);
+  // luego buscamos en la api el id de Rick
+  fetchData(api + data1.results[0].id, (error2, data2) => {
+    // si error, matamos retornando un error
+    if(error2) return console.error(error2);
+    // por ultimo la consulta a la api que contiene su dimension
+    fetchData(data2.origin.url, (error3, data3) => {
       // si error, matamos retornando un error
-      if(error2) return console.error(error2);
-      // por ultimo la consulta a la api que contiene su dimension
-      fetchData(data2.origin.url, (error3, data3) => {
-        // si error, matamos retornando un error
-        if(error3) return console.error(error3);
-        
-        // mostramos los resultados :) 
-        console.log(data1.info.count);
-        console.log(data2.name);
-        console.log(data3.dimension);
-        
-        // rutas de las peticiones en orden
-        console.log(api);
-        console.log(api + data1.results[0].id); 
-        console.log(data2.origin.url); 
+      if(error3) return console.error(error3);
       
-      });
-    });
+      // mostramos los resultados :) 
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+      
+      // rutas de las peticiones en orden
+      console.log(api);
+      console.log(api + data1.results[0].id); 
+      console.log(data2.origin.url); 
     
+    });
   });
 });
 
@@ -66,10 +61,10 @@ function idgenerator(howmany){
   var idlist = []
   for (let index = 0; index < howmany; index++) {
     // console.log((Math.round(Math.random()*826)))
-    // console.log(howmany)
+    console.log(howmany)
     idlist.push((Math.round(Math.random()*826)))
   }
-  // console.log(idlist)
+  console.log(idlist)
 }
 
 idgenerator(10);
