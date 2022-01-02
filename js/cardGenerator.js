6a0616a0b02d7134e43ac854726b9751fc14b841
samuelceron <samceronm@gmail.com>
2022-01-02 10:55:44 -0600

    Adding cards and test with two characters

---
+++ js/cardGenerator.js
@@ -0,0 +1,17 @@
+export const buildCard = (character) => {
+    const card = document.createElement("div");
+    // card.classList.add("card");
+  
+    card.innerHTML = `
+    <div class="row justify-content-right">
+      <div class="card" style="margin-left: 1rem; margin-right: 1 rem;">
+        <div class="card-body">
+            <img src="${character.image}" alt="${character.name}" class="character" />
+            <h6 class="card-title text-truncate" style="max-width: 150px;">${(character.name)}</h5>
+            <p class="card-text">${character.species}</p>
+        </div>
+      </div>
+    </div>
+    `;
+    return card;
+  }
--- js/index.js
+++ js/index.js
@@ -1,66 +1,51 @@
-// importamos el modulo para hacer las peticiones
-// let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-// direccion de la API
-let api = 'https://rickandmortyapi.com/api/character/';
+import { fetchDataCallback } from "./fetchDataCallback.js";
+import { buildCard } from "./cardGenerator.js";
 
-// funcion principal
-function fetchData(url_api, callback){
-  // instanciamos la conexion
-  let xhttp = new XMLHttpRequest();
-  // abrir una conexion con el metodo, la ruta y si es asincrono
-  xhttp.open('GET', url_api, true);
-  // validacion del llamado
-  xhttp.onreadystatechange = (event) => {
-    // el state 4 es el ultimo de la peticion
-    if(xhttp.readyState === 4){
-      // verificamos que el status este en 200, que dice que todo bien, no un 400 o 500
-      if(xhttp.status === 200){
-        // el primer valor es el err, y el siguiente el resultado
-        // ejecutamos el callback con el resultado
-        callback(null, JSON.parse(xhttp.responseText));
-      } else {
-        // si no es 200
-        let error = new Error('Error: ' + url_api);
-        // matamos el proceso con un error
-        return callback(error, null);
-      }
-    }
-  }
-  // por ultimo enviamos la peticion
-  xhttp.send();
-}
+const api = 'https://rickandmortyapi.com/api/character/1,183';
 
-// primero buscamos la lista de personajes
-fetchData(api, (error1, data1) => {
-  // si error, matamos retornando un error
-  var ids = [0,0,2,4]
-  ids.forEach(element => {
-    console.log(element)
-    if(error1) return console.error(error1);
-    // luego buscamos en la api el id de Rick
-    fetchData(api + data1.results[element].id, (error2, data2) => {
-      // si error, matamos retornando un error
-      if(error2) return console.error(error2);
-      // por ultimo la consulta a la api que contiene su dimension
-      fetchData(data2.origin.url, (error3, data3) => {
-        // si error, matamos retornando un error
-        if(error3) return console.error(error3);
-        
-        // mostramos los resultados :) 
-        console.log(data1.info.count);
-        console.log(data2.name);
-        console.log(data3.dimension);
-        
-        // rutas de las peticiones en orden
-        console.log(api);
-        console.log(api + data1.results[0].id); 
-        console.log(data2.origin.url); 
-      
-      });
-    });
-    
-  });
+//Callback
+fetchDataCallback(api, (error,data)=>{
+  if(error) return console.error;
+  console.log(data)
+  data.map(character =>{
+    const card = buildCard(character);
+    cards_container.appendChild(card);
+  })
 });
+
+
+
+// // primero buscamos la lista de personajes
+// fetchData(api, (error1, data1) => {
+//   // si error, matamos retornando un error
+//   var ids = [0,0,2,4]
+//   ids.forEach(element => {
+//     console.log(element)
+//     if(error1) return console.error(error1);
+//     // luego buscamos en la api el id de Rick
+//     fetchData(api + data1.results[element].id, (error2, data2) => {
+//       // si error, matamos retornando un error
+//       if(error2) return console.error(error2);
+//       // por ultimo la consulta a la api que contiene su dimension
+//       fetchData(data2.origin.url, (error3, data3) => {
+//         // si error, matamos retornando un error
+//         if(error3) return console.error(error3);
+        
+//         // mostramos los resultados :) 
+//         console.log(data1.info.count);
+//         console.log(data2.name);
+//         console.log(data3.dimension);
+        
+//         // rutas de las peticiones en orden
+//         console.log(api);
+//         console.log(api + data1.results[0].id); 
+//         console.log(data2.origin.url); 
+      
+//       });
+//     });
+    
+//   });
+// });
 
 function idgenerator(howmany){
   var idlist = []
